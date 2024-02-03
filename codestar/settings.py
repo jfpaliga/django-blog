220ab2983d66a5dcc11961880e6779612e67863e
James Paliga <jamespaliga@hotmail.com>
2024-02-03 13:52:16 +0000

    Install gunicorn and change settings for deployment to Heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pr67sofeoo_^q2-o8a*n1*nei&+8tf+b#1800oejw$1=-xpf8m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com',]
 
 
 # Application definition
