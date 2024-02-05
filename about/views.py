11fddcc7113eba159e4085a3117de15ae9ceae84
James Paliga <jamespaliga@hotmail.com>
2024-02-05 16:36:31 +0000

    Add about page using django view model template architecture

---
+++ about/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import About
+
+# Create your views here.
+
+def about(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by("-updated_on").first()
+
+    return render(
+        request,
+        "about.html",
+        {"about": about},
+    )
