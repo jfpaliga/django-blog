43bae58f3a06c99a91e065a54cf1f9270eed1e65
James Paliga <jamespaliga@hotmail.com>
2024-02-09 20:06:09 +0000

    Tidy up code for final deployment

--- about/admin.py
+++ about/admin.py
@@ -6,7 +6,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
+    """
+    Adds rich-text editing of content in admin
+    """
     list_display = ('title', 'updated_on')
     summernote_fields = ('body',)
 
@@ -19,5 +21,7 @@ class AboutAdmin(SummernoteModelAdmin):
 
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
+    """
+    Lists message and read fields for display in admin
+    """
     list_display = ('message', 'read',)
--- about/views.py
+++ about/views.py
@@ -7,7 +7,18 @@ from .forms import CollaborateForm
 
 def about(request):
     """
-    Renders the About page
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collaborate_form``
+        An instance of :form:`about.CollaborateForm`.
+
+    **Template:**
+    :template:`about/about.html`
     """
     about = About.objects.all().order_by("-updated_on").first()
     collaborate_form = CollaborateForm()
